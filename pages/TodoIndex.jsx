6f41a6b5f1fcef480db5b83bfb34526b89a9df0a
ALONGOD <alongoddard1@gmail.com>
2024-07-08 13:17:49 +0300

    Todos in Redux

--- pages/TodoIndex.jsx
+++ pages/TodoIndex.jsx
@@ -3,13 +3,16 @@ import { TodoList } from "../cmps/TodoList.jsx"
 import { DataTable } from "../cmps/data-table/DataTable.jsx"
 import { todoService } from "../services/todo.service.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { loadTodos, removeTodo, saveTodo } from "../store/todo.actions.js"
+import { SET_FILTER_BY } from "../store/store.js"
 
+const { useSelector, useDispatch } = ReactRedux
 const { useState, useEffect } = React
 const { Link, useSearchParams } = ReactRouterDOM
 
 export function TodoIndex() {
-
-    const [todos, setTodos] = useState(null)
+    const dispatch = useDispatch()
+    const todos = useSelector(state => state.todos)
 
     // Special hook for accessing search-params:
     const [searchParams, setSearchParams] = useSearchParams()
@@ -17,40 +20,38 @@ export function TodoIndex() {
     const defaultFilter = todoService.getFilterFromSearchParams(searchParams)
 
     const [filterBy, setFilterBy] = useState(defaultFilter)
+    // const filterBy= useSelector(state=>state.filterBy)
+
+
+    // useEffect(()=>
+    //         dispatch({ type: SET_FILTER_BY, filterBy: defaultFilter })
+    // ,[])
+
 
     useEffect(() => {
         setSearchParams(filterBy)
-        todoService.query(filterBy)
-            .then(todos => setTodos(todos))
-            .catch(err => {
-                console.eror('err:', err)
-                showErrorMsg('Cannot load todos')
-            })
+        loadTodos()
+            .catch(err => { showErrorMsg('Cannot load todos') })
     }, [filterBy])
 
+    // function setFilterBy(filterBy) {
+    //     dispatch({ type: SET_FILTER_BY, filterBy })
+
+    // }
+
+
     function onRemoveTodo(todoId) {
-        todoService.remove(todoId)
-            .then(() => {
-                setTodos(prevTodos => prevTodos.filter(todo => todo._id !== todoId))
-                showSuccessMsg(`Todo removed`)
-            })
-            .catch(err => {
-                console.log('err:', err)
-                showErrorMsg('Cannot remove todo ' + todoId)
-            })
+        removeTodo(todoId)
+            .then(() => { showSuccessMsg(`Todo removed`) })
+            .catch(err => { showErrorMsg('Cannot remove todo ' + todoId) })
     }
 
     function onToggleTodo(todo) {
         const todoToSave = { ...todo, isDone: !todo.isDone }
-        todoService.save(todoToSave)
-            .then((savedTodo) => {
-                setTodos(prevTodos => prevTodos.map(currTodo => (currTodo._id !== todo._id) ? currTodo : { ...savedTodo }))
-                showSuccessMsg(`Todo is ${(savedTodo.isDone)? 'done' : 'back on your list'}`)
-            })
-            .catch(err => {
-                console.log('err:', err)
-                showErrorMsg('Cannot toggle todo ' + todoId)
-            })
+        saveTodo(todoToSave)
+            .then(savedTodo => { showSuccessMsg(`Todo is ${(savedTodo.isDone) ? 'done' : 'back on your list'}`) })
+            .catch(err => showErrorMsg('Cannot toggle todo ')
+            )
     }
 
     if (!todos) return <div>Loading...</div>
--- store/store.js
+++ store/store.js
@@ -1,9 +1,20 @@
+import { todoService } from "../services/todo.service"
+
 const { createStore } = Redux
 
+
 export const INCREMENT = 'INCREMENT'
+export const SET_TODOS = 'SET_TODOS'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const ADD_TODO = 'ADD_TODO'
+export const UPDATE_TODO = 'UPDATE_TODO'
+
+export const SET_FILTER_BY = 'SET_FILTER_BY'
+
 const initialState = {
     counter: 100,
-    todos: []
+    todos: [],
+    // filterBy: todoService.getDefaultFilter()
 }
 
 function appReducer(state = initialState, action = {}) {
@@ -11,6 +22,26 @@ function appReducer(state = initialState, action = {}) {
 
         case INCREMENT:
             return { ...state, counter: state.counter + 1 }
+
+        case SET_TODOS:
+            return { ...state, todos: action.todos }
+
+        case REMOVE_TODO:
+            var todos = state.todos.filter(todo => todo._id !== action.todoId)
+            return { ...state, todos }
+
+        case ADD_TODO:
+            return { ...state, todos: [...state.todos, action.todo] }
+
+        case UPDATE_TODO:
+            var todos = state.todos.map(todo => todo._id === action.todo._id ? action.todo : todo)
+            return { ...state, todos }
+
+        case SET_FILTER_BY:
+            return { ...state, filterBy: action.filterBy }
+
+
+
         default:
             return state
 
